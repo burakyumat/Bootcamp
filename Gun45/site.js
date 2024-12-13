bc842a711895887ed552fcd143c3089a3cc54548
Burak Yumat <burakyumat@gmail.com>
2024-12-13 22:10:50 +0300

    devam

---
+++ Gun45/site.js
@@ -0,0 +1,11 @@
+/* DEMO AMAÇLIDIR SONRA SİLİNİZ!! */
+document.getElementById("ekle").onclick = () => {
+    for (let i = 0; i < 100; i++) {
+      document.getElementById(
+        "liste"
+      ).innerHTML += `<button type="button" class="list-group-item list-group-item-action">
+              Not ${i + 5}
+            </button>`;
+    }
+  };
+  /* <<<<< DEMO BİTİŞ ! SONRA SİLİNİZ >>> */
