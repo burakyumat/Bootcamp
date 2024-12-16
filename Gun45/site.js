95cb281e35b7c9dcaf5a91a1ee0c0de1b2ed3a15
Burak Yumat <burakyumat@gmail.com>
2024-12-16 20:01:47 +0300

    devam

--- Gun45/site.js
+++ Gun45/site.js
@@ -1,11 +1,8 @@
-/* DEMO AMAÇLIDIR SONRA SİLİNİZ!! */
+/* <<< DEMO BAŞLANGIÇ ! SONRA SİLİNİZ >>> */
 document.getElementById("ekle").onclick = () => {
-    for (let i = 0; i < 100; i++) {
-      document.getElementById(
-        "liste"
-      ).innerHTML += `<button type="button" class="list-group-item list-group-item-action">
-              Not ${i + 5}
-            </button>`;
-    }
-  };
-  /* <<<<< DEMO BİTİŞ ! SONRA SİLİNİZ >>> */
+  for (let i = 0; i < 100; i++) {
+      document.getElementById("liste").innerHTML += 
+          `<button type="button" class="list-group-item list-group-item-action">Not ${i+5}</button>`;
+  }
+};
+/* <<< DEMO   BİTİŞ   ! SONRA SİLİNİZ >>> */
