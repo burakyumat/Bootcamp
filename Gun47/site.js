d8b9b75d789af3c69053cf90a44b12ab67e918ce
Burak Yumat <burakyumat@gmail.com>
2024-12-23 21:43:30 +0300

    devam

--- Gun47/site.js
+++ Gun47/site.js
@@ -1,8 +1,8 @@
 /* <<< DEMO BAŞLANGIÇ ! SONRA SİLİNİZ >>> */
 document.getElementById("ekle").onclick = () => {
   for (let i = 0; i < 100; i++) {
-    document.getElementById("liste").innerHTML +=
-      `<button type="button" class="list-group-item list-group-item-action">Not ${i + 5}</button>`;
+      document.getElementById("liste").innerHTML += 
+          `<button type="button" class="list-group-item list-group-item-action">Not ${i+5}</button>`;
   }
 };
 /* <<< DEMO   BİTİŞ   ! SONRA SİLİNİZ >>> */
@@ -10,16 +10,17 @@ document.getElementById("ekle").onclick = () => {
 const offcanvas = new bootstrap.Offcanvas("#offcanvasExample");
 const divMenu = document.getElementById("menu");
 const divSidebarColumn = document.getElementById("sidebarColumn");
-const divOffcanvasBody = document.getElementById("offcanvasbody");
-
-window.onresize = menuAyarla;
+const divOffcanvasBody = document.getElementById("offcanvasBody");
 
 function menuAyarla() {
   if (window.innerWidth < 576) {
-    divOffcanvasBody.append(divMenu);
+      divOffcanvasBody.append(divMenu);
   }
   else {
-    divSidebarColumn.append(divMenu);
-    offcanvas.hide();
+      divSidebarColumn.append(divMenu);
+      offcanvas.hide();
   }
 };
+
+window.onresize = menuAyarla;
+menuAyarla();
